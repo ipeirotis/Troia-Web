b1af2bebb024d877b0dbd9c88976f0078bd2bd61
Michal Borysiak <borysiam@gmail.com>
2012-08-16 17:56:12 +0200

    Changes in the fabric and nginx for staging in other servers.

--- fabfile.py
+++ fabfile.py
@@ -1,54 +1,58 @@
 import os
 
-from fabric import colors, api
+from fabric import colors
+from fabric.api import cd, env, prefix, run, sudo, task
 
 
-USER_NAME = "dsas"
-USER_HOME = os.path.join("/home", USER_NAME)
-HOST_NAME = "178.32.233.139"
-HOST_NAME = "ci.10clouds.com"
-PROJECT_NAME = "Troia-Web"
-PROJECT_ROOT = os.path.join(USER_HOME, PROJECT_NAME)
+PROJECT_NAME = "troia-web"
+PROJECTS_ROOT = "/data/projects"
+PROJECT_ROOT = "{}/{}".format(PROJECTS_ROOT, PROJECT_NAME)
+STATIC_ROOT = "{}/static".format(PROJECT_ROOT)
+SOURCE_ROOT = "{}/source".format(PROJECT_ROOT)
 ENVIRONMENT_NAME = "virtualenv"
-STATIC_ROOT = os.path.join(USER_HOME, "static")
 SOURCE = "source {}".format(
         os.path.join(PROJECT_ROOT, ENVIRONMENT_NAME, "bin", "activate"))
-LOGIN = "{}@{}".format(USER_NAME, HOST_NAME)
 REPOSITORY = "git@github.com:10clouds/Troia-Web.git"
 LESSC = "lessc"
+
+USER = "{0}:{0}".format(env.user)
+
 
 def message(msg, *args):
     print colors.cyan("==>", bold=True), msg.format(*args)
 
-def prepare():
-    message(colors.blue("Preparing directories"))
-    with api.cd(STATIC_ROOT):
-        api.run("mkdir -p logs/nginx/")
-        api.run("mkdir -p services/nginx/")
 
-def clone(repository):
-    message(colors.yellow("Clonning repository"))
-    with api.cd(USER_HOME):
-        api.run("git clone {} {}".format(REPOSITORY, PROJECT_ROOT))
+def setmode(path, recursive=False, perms=None, owner=None):
+    recursive = "--recursive" if recursive else ""
+    if perms:
+        sudo("chmod {} {} {}".format(recursive, perms, path))
+    if owner:
+        sudo("chown {} {} {}".format(recursive, owner, path))
+
 
 def update():
     message(colors.blue("Updating repository"))
-    with api.cd(PROJECT_ROOT):
-        api.run("git stash")
-        api.run("git pull --rebase")
-        api.run("git diff")
+    with cd(SOURCE_ROOT):
+        run("git stash")
+        run("git pull --rebase")
+        run("git diff")
+
 
 def create_env():
     message(colors.blue("Creating virtual environment"))
-    with api.cd(PROJECT_ROOT):
-        api.run("virtualenv --python=\"/usr/bin/python2.7\" --no-site-packages "
-                "{}".format(ENVIRONMENT_NAME))
+    with cd(PROJECT_ROOT):
+        sudo("virtualenv --python=\"/usr/bin/python2.7\" --no-site-packages "
+             "{}".format(ENVIRONMENT_NAME))
+        setmode("{}/{}".format(PROJECT_ROOT, ENVIRONMENT_NAME),
+                recursive=True, owner=USER)
+
 
 def update_env():
     message(colors.blue("Installing requirements"))
-    with api.cd(PROJECT_ROOT):
-        with api.prefix(SOURCE):
-            api.run('pip install -r requirements.txt')
+    with cd(SOURCE_ROOT):
+        with prefix(SOURCE):
+            run('pip install -r requirements.txt')
+
 
 def compile(input_dir, output_dir, files):
     """ Compiles less and moves resultant css to another directory for
@@ -56,33 +60,46 @@ def compile(input_dir, output_dir, files):
     message(colors.blue("Compiling less from {} to {}".format(input_dir,
                         output_dir)))
     message(colors.blue("Files to compile: {}".format(", ".join(files))))
-    with api.cd(input_dir):
+    with cd(input_dir):
         for file_name in files:
             name, ext = file_name.rsplit(".", 1)
             result_name = "{}.css".format(name)
-            api.run("{} {} > {}".format(LESSC, file_name,
-                                        os.path.join(output_dir, result_name)))
+            run("{} {} > {}".format(LESSC, file_name,
+                                    os.path.join(output_dir, result_name)))
+
 
 def generate():
     message(colors.blue("Generating static content"))
-    with api.prefix(SOURCE):
-        api.run("hyde -g -s \"{}\" -d \"{}\"".format(PROJECT_ROOT, STATIC_ROOT))
-    prepare()
+    # Ensure static subdirectory exists.
+    sudo("mkdir -p {}".format(STATIC_ROOT))
+    setmode(STATIC_ROOT, recursive=True, owner=USER)
+    with prefix(SOURCE):
+        run("hyde -g -s \"{}\" -d \"{}\"".format(SOURCE_ROOT, STATIC_ROOT))
+    # Prepares directories structure for nginx.
+    message(colors.blue("Preparing directories structure"))
+    with cd(STATIC_ROOT):
+        run("mkdir -p logs/nginx/")
+        run("mkdir -p services/nginx/")
 
-@api.hosts(LOGIN)
+
+@task
 def initialize():
-    clone(REPOSITORY)
+    message(colors.yellow("Initializing project structure"))
+    sudo("mkdir -p {}".format(SOURCE_ROOT))
+    setmode(SOURCE_ROOT, recursive=True, owner=USER)
+    run("git clone {} {}".format(REPOSITORY, SOURCE_ROOT))
     create_env()
     update_env()
 
-@api.hosts(LOGIN)
-def deploy(update_env=False):
+
+@task
+def deploy(update_environment=False):
     update()
-    if update_env:
+    if update_environment:
         update_env()
-    media_root = os.path.join(PROJECT_ROOT, "media")
+    media_root = os.path.join(SOURCE_ROOT, "media")
     css_dir = os.path.join(media_root, "css")
-    api.run("mkdir -p {}".format(css_dir))
+    run("mkdir -p {}".format(css_dir))
     compile(os.path.join(media_root, "less"), css_dir,
             ("troia.less", "bootstrap.less",))
     generate()
